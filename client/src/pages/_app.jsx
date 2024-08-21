cd1ce036712280bc2bb63a3ddab3352bc68324eb
Alexander Li <li.alex.0901@gmail.com>
2024-08-20 18:50:06 -0700

    update favicon

--- client/src/pages/_app.jsx
+++ client/src/pages/_app.jsx
@@ -1,9 +1,14 @@
 import "@/styles/globals.scss";
 import Header from "@/components/Header";
+import Head from "next/head";
+
 import { AnimatePresence } from "framer-motion";
 export default function App({ Component, pageProps, router }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon/favicon.ico" />
+      </Head>
       <Header />
       <AnimatePresence mode="wait">
         <Component key={router.route} {...pageProps} />
